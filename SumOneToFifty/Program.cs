1243d91f914500dcfd4a634052ba52f3396d4623
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:33:52 -0400

    Committed by GitUtil

---
+++ SumOneToFifty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumOneToFifty {
+    class Program {
+        static void Main(string[] args) {
+
+            var sum = 0;
+
+            for(var i = 1; i <= 50; i++) {
+
+                if(i % 5 == 0 || i % 7 == 0) {
+                    sum = sum + i;
+                }
+
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
